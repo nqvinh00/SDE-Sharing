293ff6e7229f15d5a537b776386c496c7bff8f2e
nqvinh00 <nqvinh00@gmail.com>
2020-04-08 19:37:20 +0700

    up

--- Web/Web/settings.py
+++ Web/Web/settings.py
@@ -132,3 +132,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIR = [os.path.join(BASE_DIR, "static"),]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+django_heroku.settings(locals())
