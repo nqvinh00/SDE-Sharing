f72220df107a0eb6731c676a7e71e17b6218e79a
nqvinh00 <nqvinh00@gmail.com>
2020-04-08 15:38:14 +0700

    update

--- Web/Web/wsgi.py
+++ Web/Web/wsgi.py
@@ -7,13 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
-import os
+# import os
 
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Web.settings')
+
+# application = get_wsgi_application()
+
+# from whitenoise.django import DjangoWhiteNoise
+# application = DjangoWhiteNoise(application)
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Web.settings')
-
 application = get_wsgi_application()
-
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
